2d024e92db0b74b8f014c14b400d76d8f6dd6246
twinh <twinhuang@qq.com>
2020-07-31 15:08:46 +0800

    refactor: 更新包名和目录，避免前后端冲突

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# 0.1.0 (2020-07-30)
+
+
+### Features
+
+* 增加 invisible 类 ([](https://github.com/miaoxing/mxjs-css/commit/))
+* 增加 setTheme，设置默认主题 ([](https://github.com/miaoxing/mxjs-css/commit/))
---
+++ index.js
@@ -0,0 +1,15 @@
+import styledCss from '@styled-system/css';
+import {css as emotionCss} from '@emotion/core';
+
+export const sx = (style) => props => {
+  return emotionCss(styledCss(style)(props));
+};
+
+// float
+export const floatLeft = () => emotionCss({float: 'left'});
+export const floatRight = () => emotionCss({float: 'right'});
+export const floatNone = () => emotionCss({float: 'none'});
+
+// visibility
+export const visible = () => emotionCss({visibility: 'visible'});
+export const invisible = () => emotionCss({visibility: 'hidden'});
