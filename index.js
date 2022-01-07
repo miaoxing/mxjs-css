5d7d9fc90808d67ee7bb864c37d0fb0f1a92403a
twinh <twinhuang@qq.com>
2022-01-07 23:24:28 +0800

    feat(css): 引入 `@fower/preset-web` 作为默认配置

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-import {getConfig} from '@fower/core';
+import {getConfig, setConfig} from '@fower/core';
+import presetWeb from '@fower/preset-web';
 
 export {css, createStyle} from '@fower/core';
+
+if (getConfig().plugins.length === 0) {
+  // 加载默认配置，但是不覆盖已有的配置
+  setConfig(presetWeb);
+}
 
 /**
  * @experimental 用于生成负数，考虑其他支持方式
